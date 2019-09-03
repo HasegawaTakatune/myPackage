ab253552e420bc5cb4c088a89d7b6f8c95419fd1
cDGJ3vgijc <m.touyama@eaz.co.jp>
2019-09-03 17:59:02 +0900

    消えるボタン・俯瞰マップ・スマホのセンサー系取得のサンプル追加

---
+++ myPackage/Assets/Sensor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sensor : MonoBehaviour
+{
+    private Vector3 acceleration;
+    private Compass compass;
+    private Quaternion gyro;
+
+    [SerializeField] private Text acceleTxt;
+    [SerializeField] private Text compassTxt;
+    [SerializeField] private Text gyroTxt;
+
+    void Start()
+    {
+        Input.compass.enabled = true;
+
+        Debug.Log(Input.compass.headingAccuracy + ":" + Input.compass.timestamp);
+
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        acceleration = Input.acceleration;
+        compass = Input.compass;
+        gyro = Input.gyro.attitude;
+
+        acceleTxt.text = "Acceleration:" + acceleration;
+        compassTxt.text = "Compass:" + compass;
+        gyroTxt.text = "Gyro:" + gyro;
+    }
+}
---
+++ myPackage/Assets/SubCamera/SubCameraTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubCameraTest : MonoBehaviour
+{
+
+    [SerializeField] private Camera subCamera;
+    [SerializeField] private GameObject obj;
+
+    private Rect rect;
+    private float rate = 0.01f;
+
+    [SerializeField] private LayerMask mask;
+
+    void Start()
+    {
+        rect = subCamera.rect;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = subCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 15.0f,mask))
+            {
+                Instantiate(obj, hit.point, Quaternion.identity);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //rect.x += rate;
+            //rect.y += rate;
+            //subCamera.rect = rect;
+            StartCoroutine(MapActivate(true));
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //rect.x -= rate;
+            //rect.y -= rate;
+            //subCamera.rect = rect;
+            StartCoroutine(MapActivate(false));
+        }
+    }
+
+    private IEnumerator MapActivate(bool actv)
+    {
+        float end;
+
+        if (actv)
+        {
+
+            end = 0;
+
+            while (subCamera.rect.x >= end && subCamera.rect.y >= end)
+            {
+                yield return new WaitForSeconds(Time.deltaTime);
+                rect.x -= rate;
+                rect.y -= rate;
+                subCamera.rect = rect;
+            }
+            rect.x = 0;
+            rect.y = 0;
+            subCamera.rect = rect;
+        }
+        else
+        {
+
+            end = 0.7f;
+
+            while (subCamera.rect.x <= end && subCamera.rect.y <= end)
+            {
+                yield return new WaitForSeconds(Time.deltaTime);
+                rect.x += rate;
+                rect.y += rate;
+                subCamera.rect = rect;
+            }
+        }
+
+
+    }
+}
